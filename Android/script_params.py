6e4edc04417e2277508a2daf8b968825bd912b5f
zhulixin <lixin.zhu@quvideo.com>
2018-08-23 16:17:07 +0800

    re-upload

---
+++ Android/script_params.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""python命令行参数处理"""
+import argparse
+from appium import webdriver
+
+
+# Android脚本请加上下面这一段，用于命令行参数解析
+def params_init():
+    """从命令行获取参数，初始化"""
+    parser = argparse.ArgumentParser(
+        description='***Appium Python-Client Script Paramas***')
+    parser.add_argument(
+        '-p', '--port', help='The port for appium to listen on')
+    parser.add_argument(
+        '-pl', '--platform', help='Device system platform')
+    parser.add_argument(
+        '-v', '--version', help='Device OS version')
+    parser.add_argument(
+        '-pk', '--package', help='Package of the Android_old app you want to run')
+    parser.add_argument(
+        '-ac', '--activity', help='Activity you want to launch')
+
+    args = parser.parse_args()
+    remote_url = 'http://localhost:{}/wd/hub'.format(args.port)
+
+    desired_caps = {
+        'appPackage': args.package,
+        'appActivity': args.activity,
+        'platformName': args.platform,
+        'platformVersion': args.version,
+        'unicodeKeyboard': True,
+        'resetKeyboard': True,
+        'noReset': True
+    }
+
+    print('Params init!!!')
+    drivers = webdriver.Remote(command_executor=remote_url,
+                               desired_capabilities=desired_caps)
+    return drivers
+
+
+driver = params_init()
